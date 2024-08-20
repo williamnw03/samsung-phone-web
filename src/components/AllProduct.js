00ee7396571fb912b0f4a4a576375848b572d015
WilliamNW <williamnw943@gmail.com>
2024-08-20 22:00:54 +0700

    change api url

--- src/components/AllProduct.js
+++ src/components/AllProduct.js
@@ -1,187 +1,198 @@
-import { useEffect, useState } from "react"
-import "../style/all-card.css"
-import CardProduct from "./CardProduct"
-import DetailsCard from "./DetailsCard"
-import samsung1Img from "../img/samsung1.jpg"
+import { useEffect, useState } from "react";
+import "../style/all-card.css";
+import CardProduct from "./CardProduct";
+import DetailsCard from "./DetailsCard";
+import samsung1Img from "../img/samsung1.jpg";
 
 const AllProduct = () => {
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
 
-    useEffect(() => {
-        window.scroll(0, 0)
-    }, [])
+  const [productData, setProductData] = useState("");
+  const [detailsData, setDetailsData] = useState({
+    phone_name: "",
+    dimension: "",
+    release_date: "",
+    os: "",
+    storage: "",
+    image: "",
+    slug: "",
+  });
+  const [cancelFetch, setCancelFetch] = useState(() => {
+    return () => false;
+  });
 
-    const [productData, setProductData] = useState("");
-    const [detailsData, setDetailsData] = useState({
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const [errorDC, setErrorDC] = useState(false);
+
+  useEffect(() => {
+    const abortCards = new AbortController();
+    const fetching = async () => {
+      try {
+        const resp = await fetch(
+          "https://phone-specs-g57ftrc7l-azharimms-projects.vercel.app/brands/samsung-phones-9",
+          { signal: abortCards.signal }
+        );
+
+        if (!resp.ok) throw new Error("Something went wrong");
+
+        const data = await resp.json();
+
+        const phones = data.data.phones.map((e) => {
+          return {
+            name: e.phone_name,
+            image: e.image,
+            slug: e.slug,
+          };
+        });
+
+        setProductData((prev) => phones);
+        setLoading(false);
+        setError(false);
+      } catch (err) {
+        if ((err.message = "Failed to fetch")) {
+          console.log("Fallback API error");
+          const phones = Array(20).fill({
+            name: "This is Samsung Phone",
+            image: samsung1Img,
+            slug: "samsung1",
+          });
+          setProductData((prev) => phones);
+          setLoading(false);
+          setError(false);
+          return false;
+        }
+
+        if (err.name !== "AbortError") {
+          setError(true);
+          setLoading(false);
+          setProductData((prev) => "");
+        }
+      }
+    };
+
+    fetching();
+
+    return () => abortCards.abort();
+  }, []);
+
+  const openCard = (slug) => {
+    setDetailsData(() => {
+      return {
         phone_name: "",
         dimension: "",
         release_date: "",
         os: "",
         storage: "",
         image: "",
-        slug: ""
-    })
-    const [cancelFetch, setCancelFetch] = useState(() => {
-        return () =>  false
-    })
+        slug: "",
+      };
+    });
 
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
+    const cancelFetch = new AbortController();
 
-    const [errorDC, setErrorDC] = useState(false);
-    
-    useEffect(() => {
-        const abortCards = new AbortController()
-        const fetching = async () => {
-            
-            try {
-                const resp = await fetch("https://phonewise.onrender.com/api/search?q=samsung", {signal: abortCards.signal})
+    const getDetails = async () => {
+      setCancelFetch(() => () => cancelFetch.abort());
+      try {
+        const resp = await fetch(
+          `https://phone-specs-g57ftrc7l-azharimms-projects.vercel.app/${slug}`,
+          { signal: cancelFetch.signal }
+        );
 
-                if(!resp.ok) throw new Error("Something went wrong")
+        if (!resp.ok) throw new Error("Something went wrong");
 
-                const data = await resp.json()
-                
-                const phones = data.map(e => {
-                    return {
-                        name: e.phone_name,
-                        image: e.image,
-                        slug: e.slug
-                    }
-                })
-    
-                setProductData((prev) => phones)
-                setLoading(false)
-                setError(false)
+        const data = await resp.json();
 
-            } catch(err) {
-                if(err.message = "Failed to fetch"){
-                    console.log("Fallback API error")
-                    const phones = Array(20).fill({
-                        name: "This is Samsung Phone",
-                        image: samsung1Img,
-                        slug: "samsung1"
-                    })
-                    setProductData((prev) => phones)
-                    setLoading(false)
-                    setError(false)
-                    return false
-                }
+        setDetailsData((prev) => {
+          return {
+            phone_name: data.data.phone_name,
+            dimension: data.data.dimension,
+            release_date: data.data.release_date,
+            os: data.data.os,
+            storage: data.data.storage,
+            image: data.data.thumbnail,
+            slug: slug,
+          };
+        });
 
-                if(err.name !== "AbortError") {
-                    setError(true)
-                    setLoading(false)
-                    setProductData((prev) => "")
-                }
-            }
-        }
-
-        fetching()
-
-        return () => abortCards.abort()
-    }, [])
-
-    const openCard = (slug) => {
-
-        setDetailsData(() => {
+        setErrorDC(false);
+      } catch (err) {
+        if ((err.message = "Failed to fetch")) {
+          console.log("Fallback API error 2");
+          setErrorDC(true);
+          setDetailsData((prev) => {
             return {
-                phone_name: "",
-                dimension: "",
-                release_date: "",
-                os: "",
-                storage: "",
-                image: "",
-                slug: ""
-            }
-        })
-
-        const cancelFetch = new AbortController()
-        
-        const getDetails = async () => {
-
-            setCancelFetch(() => () => cancelFetch.abort())
-            try {
-                
-                const resp = await fetch(`https://api-mobilespecs.azharimm.site/v2/${slug}`, {signal: cancelFetch.signal})
-
-                if(!resp.ok) throw new Error("Something went wrong")
-    
-                const data = await resp.json()
-    
-    
-                setDetailsData((prev) => {
-                    return {
-                        phone_name: data.data.phone_name,
-                        dimension: data.data.dimension,
-                        release_date: data.data.release_date,
-                        os: data.data.os,
-                        storage: data.data.storage,
-                        image: data.data.thumbnail,
-                        slug: slug
-                    }
-                    
-                })
-
-                setErrorDC(false)
-
-            } catch(err) {
-                if(err.message = "Failed to fetch"){
-                    console.log("Fallback API error 2")
-                    setErrorDC(true)
-                    setDetailsData((prev) => {
-                        return {
-                            phone_name: "Samsung Phone 1",
-                            dimension: "Sed porttitor convallis mauris, ac.",
-                            release_date: "Nulla molestie lorem a augue.",
-                            os: "Nullam at pretium diam. Nam.",
-                            storage: "Quisque maximus in eros ut.",
-                            image: samsung1Img,
-                            slug: "samsung1"
-                        }
-                        
-                    })
-                    return false
-                }
-
-                if(err.name !== "AbortError") setErrorDC(true)
-                
-            }
-
-
+              phone_name: "Samsung Phone 1",
+              dimension: "Sed porttitor convallis mauris, ac.",
+              release_date: "Nulla molestie lorem a augue.",
+              os: "Nullam at pretium diam. Nam.",
+              storage: "Quisque maximus in eros ut.",
+              image: samsung1Img,
+              slug: "samsung1",
+            };
+          });
+          return false;
         }
 
-        getDetails()
+        if (err.name !== "AbortError") setErrorDC(true);
+      }
+    };
 
-        const card = document.querySelector("div.card-details")
-        card.classList.add("card-muncul")
+    getDetails();
 
-        document.querySelector("html").classList.add("hidden")
-        document.body.classList.add("hidden")
+    const card = document.querySelector("div.card-details");
+    card.classList.add("card-muncul");
 
-        const blurEl = document.querySelectorAll(".blur")
+    document.querySelector("html").classList.add("hidden");
+    document.body.classList.add("hidden");
 
-        blurEl.forEach(e => {
-            e.classList.add("blur-muncul")
-        })
+    const blurEl = document.querySelectorAll(".blur");
 
-        const darkBg = document.querySelector("div.dark-bg")
-        darkBg.classList.add("gelap-muncul")
-    }
+    blurEl.forEach((e) => {
+      e.classList.add("blur-muncul");
+    });
 
-    return (
-        <div className="loading-notfound">
-            {loading && <p className="loading">Loading...</p>}
-            {error && <p className="error">Something went wrong</p>}
+    const darkBg = document.querySelector("div.dark-bg");
+    darkBg.classList.add("gelap-muncul");
+  };
 
-            <main className="all-product">
+  return (
+    <div className="loading-notfound">
+      {loading && <p className="loading">Loading...</p>}
+      {error && <p className="error">Something went wrong</p>}
 
-                {productData && productData.map((e, i) => {
-                    return <CardProduct key={i+1} name={e.name} image={e.image} slug={e.slug} openCard={openCard}/>
-                })}
+      <main className="all-product">
+        {productData &&
+          productData.map((e, i) => {
+            return (
+              <CardProduct
+                key={i + 1}
+                name={e.name}
+                image={e.image}
+                slug={e.slug}
+                openCard={openCard}
+              />
+            );
+          })}
 
-                <DetailsCard phone_name={detailsData.phone_name} dimension={detailsData.dimension} release_date={detailsData.dimension} os={detailsData.os} storage={detailsData.storage} image={detailsData.image} slug={detailsData.slug} setDetailsData={setDetailsData} cancelFetch={cancelFetch} errorDC={errorDC}/>
-            </main>
-        </div>
-    )
+        <DetailsCard
+          phone_name={detailsData.phone_name}
+          dimension={detailsData.dimension}
+          release_date={detailsData.dimension}
+          os={detailsData.os}
+          storage={detailsData.storage}
+          image={detailsData.image}
+          slug={detailsData.slug}
+          setDetailsData={setDetailsData}
+          cancelFetch={cancelFetch}
+          errorDC={errorDC}
+        />
+      </main>
+    </div>
+  );
+};
 
-}
-
-export default AllProduct
+export default AllProduct;
--- src/components/MoreDetails.js
+++ src/components/MoreDetails.js
@@ -1,106 +1,114 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "../style/more-details.css"
+import "../style/more-details.css";
 
 const MoreDetails = () => {
+  const { type } = useParams();
 
-    const { type } = useParams()
+  const [moreDetails, setMoreDetails] = useState("");
+  const [imageProduct, setImageProduct] = useState("");
+  const [nameProduct, setNameProduct] = useState("");
+  const [error, setError] = useState(false);
 
-    const [moreDetails, setMoreDetails] = useState("")
-    const [imageProduct, setImageProduct] = useState("")
-    const [nameProduct, setNameProduct] = useState("")
-    const [error, setError] = useState(false)
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
 
-    useEffect(() => {
-        window.scroll(0, 0)
-    }, [])
+  useEffect(() => {
+    const abortMoreDetails = new AbortController();
 
-    useEffect(() => {
-        const abortMoreDetails = new AbortController()
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(
+          `https://phone-specs-g57ftrc7l-azharimms-projects.vercel.app/${type}`,
+          { signal: abortMoreDetails.signal }
+        );
 
-        const fetchData = async () => {
+        if (!resp.ok) throw new Error("Something went wrong");
 
-            try {
-                const resp = await fetch(`https://api-mobilespecs.azharimm.site/v2/${type}`, {signal: abortMoreDetails.signal})
+        const data = await resp.json();
 
-                if(!resp.ok) throw new Error("Something went wrong")
-
-                const data = await resp.json()
-    
-                setMoreDetails(() => data.data.specifications)
-                setImageProduct(() => data.data.thumbnail)
-                setNameProduct(() => data.data.phone_name)
-                setError(false)
-            } catch(err) {
-                
-                if(err.name !== "AbortError") {
-                    
-                    setError(true)
-                    setMoreDetails(() => "")
-                    setImageProduct(() => "")
-                    setNameProduct(() => "")
-                
-                }
-            }
+        setMoreDetails(() => data.data.specifications);
+        setImageProduct(() => data.data.thumbnail);
+        setNameProduct(() => data.data.phone_name);
+        setError(false);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          setError(true);
+          setMoreDetails(() => "");
+          setImageProduct(() => "");
+          setNameProduct(() => "");
         }
+      }
+    };
 
-        fetchData()
+    fetchData();
 
-        return () => abortMoreDetails.abort()
-    }, [])
+    return () => abortMoreDetails.abort();
+  }, []);
 
-    return (
-        <>
-            <div className="wrapper-details-table">
-                <main className="details-table">
+  return (
+    <>
+      <div className="wrapper-details-table">
+        <main className="details-table">
+          <div className="image-product-details-table">
+            {imageProduct ? (
+              <img src={imageProduct} alt="Product Image" />
+            ) : (
+              <div className="loading-image-MoreDetails"></div>
+            )}
+          </div>
 
+          {nameProduct ? (
+            <h1>{nameProduct}</h1>
+          ) : (
+            <h1 className="loading-MoreDetails">Loading...</h1>
+          )}
 
-                    <div className="image-product-details-table">
-                        {imageProduct ? <img src={imageProduct} alt="Product Image" /> : <div className="loading-image-MoreDetails"></div>}
-                    </div>
-
-                    {nameProduct ? <h1>{nameProduct}</h1> : <h1 className="loading-MoreDetails">Loading...</h1>}
-
-                    {error && <div className="error-more-details">
-                        <p>Something went wrong</p>
-                    </div>}
-
-                    <div className="all-details-table">
-
-                        {moreDetails && moreDetails.map((e, i) => {
-
-                            return (
-                                <div className="each-details-table" key={i}>
-                                    <h2 className="details-subtitle">{e.title}</h2>
-                                    <table>
-                                        <tbody>
-
-                                            {e.specs.map((e, i) => {
-    
-                                                return (
-                                                <tr key={i}>
-                                                    <td>{e.key}</td>
-                                                    <td>
-                                                        {e.val.length > 1 ? <ul className="val-ul">{e.val.map((e, i) => <li key={i}><span>{e}</span></li>)}
-                                                        </ul> : e.val}
-                                                    </td>
-                                                </tr>
-                                                )
-                                            })}
-                                        </tbody>
-                                    </table>
-                                </div>
-                            )
-
-                        })}
-
-                    </div>
-                
-                </main>
+          {error && (
+            <div className="error-more-details">
+              <p>Something went wrong</p>
             </div>
-        </>
-    )
+          )}
 
-}
+          <div className="all-details-table">
+            {moreDetails &&
+              moreDetails.map((e, i) => {
+                return (
+                  <div className="each-details-table" key={i}>
+                    <h2 className="details-subtitle">{e.title}</h2>
+                    <table>
+                      <tbody>
+                        {e.specs.map((e, i) => {
+                          return (
+                            <tr key={i}>
+                              <td>{e.key}</td>
+                              <td>
+                                {e.val.length > 1 ? (
+                                  <ul className="val-ul">
+                                    {e.val.map((e, i) => (
+                                      <li key={i}>
+                                        <span>{e}</span>
+                                      </li>
+                                    ))}
+                                  </ul>
+                                ) : (
+                                  e.val
+                                )}
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                );
+              })}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
 
 export default MoreDetails;
